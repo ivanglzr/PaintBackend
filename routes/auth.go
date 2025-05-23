ea2bc34202dce2af69c33241a48eb13daad1fed9
Ivan <ivanglzr01@gmail.com>
2025-05-23 19:28:46 +0200

    feat: implement the delete user controller

--- routes/auth.go
+++ routes/auth.go
@@ -5,7 +5,8 @@ import (
 	"github.com/ivanglzr/PaintBackend/controllers"
 )
 
-func SetupRoutes(router *mux.Router) {
+func AuthRoutes(router *mux.Router) {
 	router.HandleFunc("/auth/log-in", controllers.LogIn).Methods("POST")
 	router.HandleFunc("/auth/register", controllers.Register).Methods("POST")
+	router.HandleFunc("/auth", controllers.DeleteUser).Methods("DELETE")
 }
